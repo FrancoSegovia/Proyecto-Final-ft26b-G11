eadc3ec3495d4aff0e4d6c6bd297c1c0f4111c66
Braian Aguada <braianaguada@gmail.com>
2022-08-03 13:00:49 -0300

    Add_Routes

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const router = Router();
 
 const ownerRoute = require('./owner')
+const deliveryRoute = require('./delivery')
+const usersRoute = require('./users')
 
 
 module.exports = router;
