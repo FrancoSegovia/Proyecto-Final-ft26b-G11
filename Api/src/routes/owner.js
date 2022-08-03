cc148982561ce2cfd9e26bd357b157123905a0f8
Braian Aguada <braianaguada@gmail.com>
2022-08-03 14:37:09 -0300

    Route_owner

---
+++ Api/src/routes/owner.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const ownerSchema = require('../schema/Owner.js')
+
+router.post("/owners", (req, res) => {
+    const owner = ownerSchema(req.body)
+    owner
+    .save().then((data) => res.json(data))
+    .catch((error) => res.json({message: error}))
+})
+
+module.exports = router;
