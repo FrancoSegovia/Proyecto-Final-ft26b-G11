280af893b2e614cb1a6a06a12d4ba6e713f15e68
emilioalvarez05 <emilioalvarez_5@hotmail.com>
2022-08-04 13:07:34 -0300

    Update mockup.js

--- Api/src/routes/mockup.js
+++ Api/src/routes/mockup.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-var myFoods = require("../schema/Foods.json")
+
+var myFoods = require("../schema/Foods.json");
+
 
 router.get("/", (req, res) => {
     const {name} = req.query
@@ -15,6 +17,17 @@ router.get("/", (req, res) => {
         }else{
             res.send(myFoods)
         }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get("/:id", (req, res) => {
+    const {id} = req.params
+    try {
+     var foodById = myFoods.filter((f) => f.id === id)
+
+     res.send(foodById)
     } catch (error) {
         console.log(error)
     }
