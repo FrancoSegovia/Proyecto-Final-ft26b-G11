20a0f328446790490e70cbb993973d1094a294c1
FrancoSegovia <87335513+FrancoSegovia@users.noreply.github.com>
2022-08-04 17:08:15 -0300

    feature/cards_container
    cards container first version commit

---
+++ client/src/roles/user/views/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Grid } from '@mui/material';
+import json from '../../../utils/foods.js';
+import UserCard from '../features/UserCards/UserCard';
+
+function Home() {
+
+  return (
+    <div style={{marginTop:"100px"}}>
+            <Grid container justifyContent="center" direction="row" rowSpacing={1} style={{marginBottom:"50px"}}>
+
+                <Grid item xs={2} style={{backgroundColor:"red", textAlign:"center"}}>
+                    <item>ACÁ VA EL COMPONENTE DE FILTROS/ORDENAMIENTOS</item>
+                </Grid>
+
+                <Grid container justifyContent="space-evenly" xs={7} style={{backgroundColor:"White", textAlign:"center"}}>
+                    {json?.foods.map(food => {
+                        return <UserCard food={food}/>
+                    })}
+                </Grid>
+
+                <Grid item xs={2} style={{backgroundColor:"Red", textAlign:"center"}}>
+                    <item>ACÁ VA EL COMPONENTE DE CARRITO</item>
+                </Grid>
+
+            </Grid>
+    </div>
+  )
+}
+
+export default Home
