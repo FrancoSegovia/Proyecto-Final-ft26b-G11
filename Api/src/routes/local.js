570a72ed8e27e892c7750ff46e0cd29a1b2fc4c8
emilioalvarez05 <emilioalvarez_5@hotmail.com>
2022-08-04 18:04:57 -0300

    route-local

--- Api/src/routes/local.js
+++ Api/src/routes/local.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const myFoods = require("../schema/Foods.json");
+const myLocal = require("../schema/Local.json");
 
 router.get("/", (req, res) => {
     const {name} = req.query
     try {
         if(name){
-            const foodByName = myFoods.filter((f) => f.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
-            if(foodByName.length){
-                return res.send(foodByName)
+            const localByName = myLocal.filter((l) => l.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
+            if(localByName.length){
+                return res.send(localByName)
             }
             res.status(404).send("No se encontro el producto")
         }else{
-            res.send(myFoods)
+            res.send(myLocal)
         }
     } catch (error) {
         console.log(error)
@@ -23,8 +23,8 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
     const {id} = req.params
     try {
-     const foodById = myFoods.find(obj => obj.id.toString() === id)
-     res.send(foodById)
+     const localById = myLocal.find(obj => obj.id.toString() === id)
+     res.send(localById)
     } catch (error) {
         console.log(error)
     }
