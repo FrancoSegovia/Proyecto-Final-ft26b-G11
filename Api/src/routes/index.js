022b9720d586f591b69510bb66500911797116b1
Braian Aguada <braianaguada@gmail.com>
2022-08-03 22:44:24 -0300

    Update index.js

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
-const router = Router();
 
 const ownerRoute = require('./owner')
 const deliveryRoute = require('./delivery')
 const usersRoute = require('./users')
+const mockup = require('./mockup')
 
+const router = Router();
+
+router.use("/mockup", mockup)
+router.use("/owner", ownerRoute)
+router.use("/delivery", deliveryRoute)
+router.use("/users", usersRoute)
 
 module.exports = router;
