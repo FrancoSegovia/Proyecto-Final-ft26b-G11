646549b25de7f07399c2e19d640f929cb4aedd93
Braian Aguada <braianaguada@gmail.com>
2022-08-03 23:26:26 -0300

    Update mockup.js

--- Api/src/routes/mockup.js
+++ Api/src/routes/mockup.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const mockupJson = require('../schema/foods.json')
+const mockupJson = require('../schema/Foods.json')
 
 router.get("/", (req, res) => {
     const mockup = mockupJson(req.body)
