88279605807e7c90eb293f3928d0ae7b797dd6bc
emilioalvarez05 <emilioalvarez_5@hotmail.com>
2022-08-08 00:21:59 -0300

    new-route-get

--- Api/src/routes/users.js
+++ Api/src/routes/users.js
@@ -13,4 +13,19 @@ router.post("/", (req, res) => {
     .catch((error) => res.json({ msg: error}));
 });
 
+router.get("/", (req, res) => {
+    const { name } = req.query;
+    if (name) {
+      userSchema
+        .find({ name: new RegExp(req.query.name.toLowerCase(), "i") })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+    } else {
+      userSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+    }
+  });
+  
 module.exports = router
