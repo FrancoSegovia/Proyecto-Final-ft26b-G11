70948270258133fdaebc5968c0ff490579e7dc31
Octavio Paz <octaviopaz02@gmail.com>
2022-08-09 15:28:54 -0300

    Cosas piolas

---
+++ client/src/roles/landing/features/AppBar/AppBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import Box from '@mui/material/Box';
+// import Link from '@mui/material/Link';
+// import AppBar from '../components/AppBar';
+// import Toolbar from '../components/Toolbar';
+
+export default function AppBar() {
+    return (<>
+        
+    </>)
+}
