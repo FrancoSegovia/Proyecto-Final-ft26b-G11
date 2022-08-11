6a7da270f98ad16946f7ad4f42f675d4cd4b4c07
Octavio Paz <octaviopaz02@gmail.com>
2022-08-11 01:50:45 -0300

    Pasarela de Pagos
    Creando elemento, añadiendo ruta, instalando stripe

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./roles/landing/views/LandingPage/LandingPage";
+import Home from "./roles/user/views/Home/Home.jsx";
 import CreateStore from "./roles/user/views/CreateStore/CreateStore.jsx";
-import Home from "./roles/user/views/Home.jsx";
+import Payment from "./roles/user/views/Payment/Payment"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route exact path="/" element={<LandingPage />} />
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/create" element={<CreateStore />} />
+        <Route exact path="/pay" element={<Payment />} />
       </Routes>
     </div>
   );
