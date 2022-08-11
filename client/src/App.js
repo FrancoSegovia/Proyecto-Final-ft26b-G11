a5d5c85c29a89927344a8000b30d29cfc1cc2505
Cesar <cesar.alcoholado@hotmail.com>
2022-08-11 12:07:01 -0300

    Added-UserSingUp-UserSignIn-Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import CreateStore from "./roles/user/views/CreateStore/CreateStore.jsx";
 import Home from './roles/user/views/Home.jsx';
+import UserSignUp from "./roles/Auth/UserSignUp.jsx";
+import UserSignIn from "./roles/Auth/UserSignIn.jsx";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { loadUser } from './redux/actions/index';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  //!no usar hasta que esté lista la ruta del back
+  // useEffect(()=> {
+  //   dispatch(loadUser());
+  // },[])
+
   return (
     <div>
       <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/create" element={<CreateStore/>} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/create" element={<CreateStore />} />
+        <Route exact path="/SignUp" element={<UserSignUp />} />
+        <Route exact path="/SignIn" element={<UserSignIn/>} />
       </Routes>
     </div>
   );
