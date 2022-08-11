64dd274a63b6fa7f6a730556e810e98fda2e3115
FrancoSegovia <87335513+FrancoSegovia@users.noreply.github.com>
2022-08-11 12:15:10 -0300

    update/refactored-dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
 import { Routes, Route } from "react-router-dom";
+
 import LandingPage from "./roles/landing/views/LandingPage/LandingPage";
+
 import CreateStore from "./roles/user/views/CreateStore/CreateStore.jsx";
 import Home from "./roles/user/views/Home.jsx";
+
+import Dashboard from "./roles/admin/views/Dashboard";
+
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/home" element={<Home />} />
+                    {/* General Views */}
+        <Route exact path="/landing" element={<LandingPage />} />
+
+                    {/* User Views */}
+        <Route exact path="/user/home" element={<Home />} />
+
         <Route exact path="/create" element={<CreateStore />} />
+
+                    {/* Admin Views */}
+        <Route exact path="/admin/dashboard" element={<Dashboard />} /> 
+
+
       </Routes>
     </div>
   );
