ecdcb39813c57ac3d1e7d8857ee147f014e0623c
Octavio Paz <octaviopaz02@gmail.com>
2022-08-12 14:47:38 -0300

    Payment and conditional Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,38 @@
+import React, { useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
+
 import LandingPage from "./roles/landing/views/LandingPage/LandingPage";
 import Home from "./roles/user/views/Home/Home.jsx";
 import CreateStore from "./roles/user/views/CreateStore/CreateStore.jsx";
-import Payment from "./roles/user/views/Payment/Payment"
+import Payment from "./roles/user/views/Payment/Payment";
 
-function App() {
-  return (
-    <div>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/home" element={<Home />} />
-        <Route exact path="/create" element={<CreateStore />} />
-        <Route exact path="/pay" element={<Payment />} />
-      </Routes>
-    </div>
-  );
+export default function App() {
+
+  //Utilizar un useEffect para traer los datos del tipo de usuario
+  //Mediante un condicional redirigirlo al Routes que le corresponda
+  //Dejar la landing y el LogIn/Register para cuando no tenga userStatus
+
+  if (true) {
+    return (
+      <div>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/create" element={<CreateStore />} />
+          <Route exact path="/pay" element={<Payment />} />
+        </Routes>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/create" element={<CreateStore />} />
+          <Route exact path="/pay" element={<Payment />} />
+        </Routes>
+      </div>
+    );
+  }
 }
-
-export default App;
--- client/src/roles/user/views/Payment/Payment.jsx
+++ client/src/roles/user/views/Payment/Payment.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
-import { Elemets, CardElement } from "@stripe/react-stripe-js";
+import { Elemets, CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 //Abajo va una key de stripe
 //const stipePromise = loadStripe("key")
@@ -17,8 +17,20 @@ export default function Payment() {
 
 //export default function PaymentForm(seguro recibe algo){
 //
+//  const stripe = useStripe()
+//  const elements = useElemets
+
 // const handleSubmit = (e) => {
 //      e.preventDefault();
+//
+//  stripe.createPaymentMethod({
+//    type:'card',
+//    card: elements.getElement(CardElement)
+//  })
+//    .then(({error, paymenMethod}) => {
+//
+//    })
+//
 //  }
 //
 // return (
