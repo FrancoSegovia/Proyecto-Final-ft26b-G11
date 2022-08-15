868355039da4dd2185239a71f4e5964139c57eb8
emilioalvarez05 <emilioalvarez_5@hotmail.com>
2022-08-15 13:45:41 -0300

    Update Cart.js

--- Api/src/schema/Cart.js
+++ Api/src/schema/Cart.js
@@ -20,7 +20,7 @@ const schema =  Schema({
     user: {
         type: Schema.Types.ObjectId,
         ref: "User",
-        required: false,
+        required: true,
       }, 
 }, { collection: "carts" } 
 );
