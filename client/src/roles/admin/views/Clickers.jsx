561ccb602411d4d567a8a371d1d7b30334838031
Octavio Paz <octaviopaz02@gmail.com>
2022-08-16 22:14:05 -0300

    Code styles

--- client/src/roles/admin/views/Clickers.jsx
+++ client/src/roles/admin/views/Clickers.jsx
@@ -1,19 +1,21 @@
-import * as React from "react";
+import React from "react";
 import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
+
 import MuiDrawer from "@mui/material/Drawer";
-import Box from "@mui/material/Box";
 import MuiAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import Badge from "@mui/material/Badge";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Link from "react-router-dom";
+import {
+  Badge,
+  Box,
+  Container,
+  CssBaseline,
+  Divider,
+  Grid,
+  IconButton,
+  List,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import NotificationsIcon from "@mui/icons-material/Notifications";
