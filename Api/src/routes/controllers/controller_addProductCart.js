fb0677f9e89393b602521070a27aaa29d1e9d24a
emilioalvarez05 <emilioalvarez_5@hotmail.com>
2022-08-17 22:21:42 -0300

    UPDATE

--- Api/src/routes/controllers/controller_addProductCart.js
+++ Api/src/routes/controllers/controller_addProductCart.js
@@ -6,52 +6,89 @@ const mongoose = require("mongoose");
 function getModelByName(name) {
     return mongoose.model(name);
   }
-
+  
+  
 const addProductCart = async (req, res) => {
+
+    const { id } = req.params
+    const { idP } = req.body
+
+    const item = getModelByName("Cart")
+    const itemsProduct = item.findOne
+
+    console.log("soyItem", item)
     
+
+    const user = await User.findById(id)
+    // console.log(user)
+
+    const product = await Product.findById(idP)
+    // console.log("soyProduct", product)
+    // console.log(product)
+
+    const cart = new Cart({
+        user: id, 
+        items: [{
+            product: idP,
+            amount: 1
+        }]
+    })
+    console.log("soy cart", cart)
     
-    const { name, image, price, user} = req.body;
    
+    const newProductInCart = await cart.save(); 
+     res.json(newProductInCart)
+
+
+
+
+
+}
     
+module.exports = addProductCart
 
-    const product = getModelByName("Product")
+  
+  
+  
+  
+  
+//   const product = getModelByName("Product")
     
-    // Nos fijamos si tenemos el producto
+//     // Nos fijamos si tenemos el producto
 
-    const productExist = await product.findOne({ name })
+//     const productExist = await product.findOne({ name })
 
-    // Nos fijamos si todos los campos vienen con info
+//     // Nos fijamos si todos los campos vienen con info
 
-    const isNotEmptyCart = name !== "" && image !== "" && price !== "" ;
+//     const isNotEmptyCart = name !== "" && image !== "" && price !== "" ;
 
-    // Nos fijamos si el producto esta en el carrito
+//     // Nos fijamos si el producto esta en el carrito
 
-    const inCart = await Cart.findOne({name});
+//     const inCart = await Cart.findOne({name});
     
-    // Si no tenemos el producto 
+//     // Si no tenemos el producto 
 
-    if(!productExist){
-        res.status(400).json({
-            message: "This product is not in our database "
-        })
+//     if(!productExist){
+//         res.status(400).json({
+//             message: "This product is not in our database "
+//         })
 
-    // Si nos envian algo y NO esta en el carrito lo agregamos    
-    } else if (isNotEmptyCart && !inCart){
-        const newProductInCart = new Cart({ name, image, price, amount: 1, user});
+//     // Si nos envian algo y NO esta en el carrito lo agregamos    
+//     } else if (isNotEmptyCart && !inCart){
+//         const newProductInCart = new Cart({ name, image, price, amount: 1, user});
 
-    // Actualizamos la prop inCart
-        product.findByIdAndUpdate(productExist?._id, { inCart: true, name, image, price}, { new: true} )
-        product.findByIdAndUpdate(!productExist, {$push: newProductInCart})
-        const newProduct = await newProductInCart.save();
-        res.json(newProduct)
+//     // Actualizamos la prop inCart
+//         product.findByIdAndUpdate(productExist?._id, { inCart: true, name, image, price}, { new: true} )
+//         const newProduct = await newProductInCart.save();
+//         res.json(newProduct)
             
         
-    }else  {
-        res.status(400).json({
-            message: "The product is in Cart"
-        })
+//     }else  {
+//         res.status(400).json({
+//             message: "The product is in Cart"
+//         })
         
-    }
-}
+//     }
+// }
 
-module.exports = addProductCart
+// module.exports = addProductCart
