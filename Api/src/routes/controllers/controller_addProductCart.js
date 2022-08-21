cca2de1157d457c1888963ed39fb778f44928a24
Octavio Paz <octaviopaz02@gmail.com>
2022-08-21 01:21:39 -0300

    Shopping cart updates

--- Api/src/routes/controllers/controller_addProductCart.js
+++ Api/src/routes/controllers/controller_addProductCart.js
@@ -5,119 +5,82 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 
 function getModelByName(name) {
-    return mongoose.model(name);
-  }
-  
-  
+  return mongoose.model(name);
+}
+
 const addProductCart = async (req, res) => {
-    
-    const { id } = req.params // USER ID
-    const { _id } = req.body // PRODUCT ID
-    
-    
-    const product = await Product.findById({_id})  
-   
-    const user = await User.update( 
-      { "_id" : id} , 
-      { "$push" : { "cart" : { "product" :  _id } } } , 
-      { "multi" : true }  
-  )
-  const userResponse = await User.aggregate([
-    {
-      $lookup: {
-        from: 'products', // He probado con 'Categories' y 'categories'
-        localField: 'cart.product', 
-        foreignField: '_id',
-        as: 'cart.products'
-      }
-    },
+  try {
+    const { id } = req.params; // USER ID
+    const { _id } = req.body; // PRODUCT ID
 
-    {
-      $match: {
-        _id: ObjectId(id)
-      }
-    },
-    {
-      "$replaceRoot": {
-        "newRoot": "$cart"
-      }
-    },
-  ])
+    const product = await Product.findById({ _id });
 
-    res.json(userResponse)
-  }
- 
-    
-
-const getCart = async (req, res) => {
-    const { id } = req.params // user
-    
-    const user = await User.aggregate([
+    const user = await User.update(
+      { _id: id },
+      { $push: { cart: { product: _id } } },
+      { multi: true }
+    );
+    const userResponse = await User.aggregate([
       {
         $lookup: {
-          from: 'products', // He probado con 'Categories' y 'categories'
-          localField: 'cart.product', 
-          foreignField: '_id',
-          as: 'cart.products'
-        }
+          from: "products",
+          localField: "cart.product",
+          foreignField: "_id",
+          as: "cart.products",
+        },
       },
 
       {
         $match: {
-          _id: ObjectId(id)
-        }
+          _id: ObjectId(id),
+        },
       },
       {
-        "$replaceRoot": {
-          "newRoot": "$cart"
-        }
+        $replaceRoot: {
+          newRoot: "$cart",
+        },
       },
-    ])
+    ]);
 
-      res.json(user)
-    }
-    
+    res.status(200).json(userResponse);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
+const getCart = async (req, res) => {
+  try {
+    const { id } = req.params; // user
+
+    const user = await User.aggregate([
+      {
+        $lookup: {
+          from: "products",
+          localField: "cart.product",
+          foreignField: "_id",
+          as: "cart.products",
+        },
+      },
+
+      {
+        $match: {
+          _id: ObjectId(id),
+        },
+      },
+      {
+        $replaceRoot: {
+          newRoot: "$cart",
+        },
+      },
+    ]);
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   addProductCart,
   getCart,
 };
-
-//   const product = getModelByName("Product")
-
-//     // Nos fijamos si tenemos el producto
-
-//     // Nos fijamos si todos los campos vienen con info
-
-//     const isNotEmptyCart = name !== "" && image !== "" && price !== "" ;
-
-//     // Nos fijamos si el producto esta en el carrito
-
-//     const inCart = await Cart.findOne({name});
-
-//     // Si no tenemos el producto
-
-//     if(!productExist){
-//         res.status(400).json({
-//             message: "This product is not in our database "
-//         })
-
-//     // Si nos envian algo y NO esta en el carrito lo agregamos
-//     } else if (isNotEmptyCart && !inCart){
-//         const newProductInCart = new Cart({ name, image, price, amount: 1, user});
-
-//     // Actualizamos la prop inCart
-//         product.findByIdAndUpdate(productExist?._id, { inCart: true, name, image, price}, { new: true} )
-//         const newProduct = await newProductInCart.save();
-//         res.json(newProduct)
-
-//     }else  {
-//         res.status(400).json({
-//             message: "The product is in Cart"
-//         })
-
-//     }
-// }
-
-// module.exports = addProductCart
--- client/src/roles/Auth/UserSignUp.jsx
+++ client/src/roles/Auth/UserSignUp.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { signUpDelivery, signUpOwner, signUpUser } from "../../redux/actions";
 import inputCheckout from "../../utils/functions/inputCheckout";
+
+import PhoneInput from "react-phone-input-2";
+import 'react-phone-input-2/lib/material.css';
 
 import {
   Avatar,
@@ -27,6 +30,7 @@ export default function SignUp() {
   const dispatch = useDispatch();
 
   const [error, setError] = useState({});
+  const [phone, setPhone] = useState("");
   const [user, setUser] = useState({
     type: "user",
     name: "",
@@ -48,8 +52,8 @@ export default function SignUp() {
     setError(inputCheckout(user));
   }, [user]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (user.type === "owner") {
       dispatch(signUpOwner(user));
     } else if (user.type === "user") {
@@ -69,18 +73,24 @@ export default function SignUp() {
     navigate("/SignIn", { replace: true });
   };
 
-  const handleChange = (event) => {
+  const handleChange = (e) => {
     setUser({
       ...user,
-      [event.target.name]: event.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
-  const handleGoogleS = (e) => {
-    console.log(e)
+  const handlePhone = (e) => {
+    if (phone.length > 10) return;
+    setPhone(e.target.value);
   };
+
+  const handleGoogleS = (e) => {
+    console.log(e);
+  };
+
   const handleGoogleE = (e) => {
-    console.log(e)
+    console.log(e);
   };
 
   const onSelect = (event) => {
@@ -92,172 +102,177 @@ export default function SignUp() {
 
   return (
     <ThemeProvider theme={theme}>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} >
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-            <LockOutlined />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Registro
-          </Typography>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <Typography component="h6" variant="subtitle2" align="center" sx={{color:"#a6a6a6"}}>"*" CAMPOS OBLIGATORIOS</Typography>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="name"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Nombre"
-                  
-                  onChange={(e) => handleChange(e)}
-                  value={user.name}
-                />
-              </Grid>
-              {/* {error.name && error.name} */}
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastname"
-                  label="Apellido"
-                  name="lastname"
-                  autoComplete="family-name"
-                  onChange={(e) => handleChange(e)}
-                  value={user.lastname}
-                />
-              </Grid>
-              {/* {error.lastname && error.lastname} */}
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Dirección de correo electrónico"
-                  name="email"
-                  autoComplete="eMail"
-                  onChange={(e) => handleChange(e)}
-                  value={user.email}
-                />
-              </Grid>
-              {/* {error.email && error.email} */}
-              {(user.type === "user" || user.type === "owner") && (
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="direction"
-                    label="Dirección"
-                    id="direction"
-                    autoComplete="new-direction"
-                    onChange={(e) => handleChange(e)}
-                    value={user.direction}
-                  />
-                </Grid>
-              )}
-
-              {user.type === "owner" && (
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    name="phone"
-                    label="Teléfono"
-                    id="phone"
-                    autoComplete="new-direction"
-                    onChange={(e) => handleChange(e)}
-                    value={user.phone}
-                  />
-                </Grid>
-              )}
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Contraseña"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  onChange={(e) => handleChange(e)}
-                  value={user.password}
-                />
-              </Grid>
-              {/* {error.password && error.password} */}
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="cPassword"
-                  label="Confirme su Contraseña"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  onChange={(e) => handleChange(e)}
-                  value={user.cPassword}
-                />
-              </Grid>
-              {error.cPassword && error.cPassword}
-              {(user.type === "delivery" || user.type === "users") && (
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="phone"
-                    label="Teléfono"
-                    id="phone"
-                    autoComplete="new-phone"
-                    onChange={(e) => handleChange(e)}
-                    value={user.phone}
-                  />
-                </Grid>
-              )}
-
-              {user.type === "delivery" && (
-                <Grid item xs={12}>
-                  <Select
-                    fullWidth
-                    value={user.vehicle}
-                    onChange={onSelect}
-                    name={"vehicle"}
-                  >
-                    <MenuItem value={"AUTO"}>Auto</MenuItem>
-                    <MenuItem value={"BICICLETA"}>Bicicleta</MenuItem>
-                    <MenuItem value={"MOTO"}>Moto</MenuItem>
-                  </Select>
-                </Grid>
-              )}
-            </Grid>
-            {/* <Link to="/landing" style={{ textDecoration: "none", color: "white" }}> */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 2, mb: 2 }}
-              disabled={
-                Object.keys(error).length ||
-                !user.email.includes("@") ||
-                !user.email.includes(".com")
-              }
+            <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+              <LockOutlined />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Registro
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
             >
-              Registrarme
-            </Button>
-            {/* <Box sx={{ mt: 0.5, mb: 3 }}>
+              <Typography
+                component="h6"
+                variant="subtitle2"
+                align="center"
+                sx={{ color: "#a6a6a6" }}
+              >
+                "*" CAMPOS OBLIGATORIOS
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="name"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Nombre"
+                    onChange={(e) => handleChange(e)}
+                    value={user.name}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastname"
+                    label="Apellido"
+                    name="lastname"
+                    autoComplete="family-name"
+                    onChange={(e) => handleChange(e)}
+                    value={user.lastname}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Dirección de correo electrónico"
+                    name="email"
+                    autoComplete="eMail"
+                    onChange={(e) => handleChange(e)}
+                    value={user.email}
+                  />
+                </Grid>
+                {/* {error.email && error.email} */}
+                {(user.type === "user" || user.type === "owner") && (
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="direction"
+                      label="Dirección"
+                      id="direction"
+                      autoComplete="new-direction"
+                      onChange={(e) => handleChange(e)}
+                      value={user.direction}
+                    />
+                  </Grid>
+                )}
+
+                {user.type === "owner" && (
+                  <Grid item xs={12}  >
+                    <PhoneInput
+                      align="center"
+                      specialLabel=""
+                      country="ar"
+                      onlyCountries={["ar"]}
+                      disableCountryCode={true}
+                      value={phone}
+                      placeholder="Teléfono"
+                      disableDropdown={true}
+                    />
+                  </Grid>
+                )}
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Contraseña"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    onChange={(e) => handleChange(e)}
+                    value={user.password}
+                  />
+                </Grid>
+                {/* {error.password && error.password} */}
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="cPassword"
+                    label="Confirme su Contraseña"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    onChange={(e) => handleChange(e)}
+                    value={user.cPassword}
+                  />
+                </Grid>
+                {error.cPassword && error.cPassword}
+                {(user.type === "delivery" || user.type === "users") && (
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="phone"
+                      label="Teléfono"
+                      id="phone"
+                      autoComplete="new-phone"
+                      onChange={(e) => handleChange(e)}
+                      value={user.phone}
+                    />
+                  </Grid>
+                )}
+
+                {user.type === "delivery" && (
+                  <Grid item xs={12}>
+                    <Select
+                      fullWidth
+                      value={user.vehicle}
+                      onChange={onSelect}
+                      name={"vehicle"}
+                    >
+                      <MenuItem value={"AUTO"}>Auto</MenuItem>
+                      <MenuItem value={"BICICLETA"}>Bicicleta</MenuItem>
+                      <MenuItem value={"MOTO"}>Moto</MenuItem>
+                    </Select>
+                  </Grid>
+                )}
+              </Grid>
+              {/* <Link to="/landing" style={{ textDecoration: "none", color: "white" }}> */}
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 2, mb: 2 }}
+                disabled={
+                  Object.keys(error).length ||
+                  !user.email.includes("@") ||
+                  !user.email.includes(".com")
+                }
+              >
+                Registrarme
+              </Button>
+              {/* <Box sx={{ mt: 0.5, mb: 3 }}>
               <GoogleLogin
                 buttonText="Registrate con Google (Proximamente) "
                 onSuccess={(e) => handleGoogleS(e)}
@@ -265,30 +280,37 @@ export default function SignUp() {
                 cookiePolicy={"single_host_origin"}
               />
             </Box> */}
-            <Grid
-              container
-              justifyContent="flex-end"
-              style={{ marginBottom: "15px" }}
-            >
-              <Grid item>
-                <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-                  <Button variant="contained" startIcon={<ArrowBack />}>
-                    Regresar
-                  </Button>
-                </Link>
+              <Grid
+                container
+                justifyContent="flex-end"
+                style={{ marginBottom: "15px" }}
+              >
+                <Grid item>
+                  <Link
+                    to="/"
+                    style={{ textDecoration: "none", color: "white" }}
+                  >
+                    <Button variant="contained" startIcon={<ArrowBack />}>
+                      Regresar
+                    </Button>
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid container justifyContent="center" style={{ marginBottom: "30px", marginTop:"30px" }}>
-              <Grid item>
-                <Link to="/SignIn" style={{ textDecoration: "none" }}>
-                  ¿Ya tienes una cuenta? ¡Inicia sesión!
-                </Link>
+              <Grid
+                container
+                justifyContent="center"
+                style={{ marginBottom: "30px", marginTop: "30px" }}
+              >
+                <Grid item>
+                  <Link to="/SignIn" style={{ textDecoration: "none" }}>
+                    ¿Ya tienes una cuenta? ¡Inicia sesión!
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </GoogleOAuthProvider>
+        </Container>
+      </GoogleOAuthProvider>
     </ThemeProvider>
   );
 }
