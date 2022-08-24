49c1dad5c947ca8ece527b40ba749332a9073b5a
FrancoSegovia <87335513+FrancoSegovia@users.noreply.github.com>
2022-08-24 04:40:17 -0300

    update

--- client/src/api/index.js
+++
@@ -1,8 +0,0 @@
-export const setHeaders = () => {
-  const header = {
-    headers: {
-      "access_token": localStorage.getItem("token")
-    }
-  }
-  return header
-}
--- client/src/roles/landing/features/AppBar/AppBar.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import MuiAppBar from "@mui/material/AppBar";
-
-export default function AppBar(props) {
-  return <MuiAppBar elevation={0} position="fixed" {...props} />;
-}
